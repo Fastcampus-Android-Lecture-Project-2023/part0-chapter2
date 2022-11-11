d9bd6991f021cee5d702b2ed1920177928bcb6f2
ethanchae <boschae36@gmail.com>
2022-11-11 23:04:12 +0900

    first commit

---
+++ app/src/main/java/fastcampus/part0/chapter2/Example4.kt
@@ -0,0 +1,16 @@
+package fastcampus.part0.chapter2
+
+import android.icu.number.IntegerWidth
+
+lateinit var text : String
+val test : Int by lazy {
+    println("초기화 중")
+    100
+}
+
+fun main() {
+    text = "안녕하세요"
+    println("메인 함수 실행")
+    println("초기화 한 값 $test")
+    println("두번째 호출 $test")
+}
